b655da93da5a4c20401ab468a96d555ffc26c0c8
Cangerana <mrjoaovictorc@gmail.com>
2021-06-27 17:44:11 -0300

    Ctr C ctr V

--- SumVectors/sumVectors.c
+++ SumVectors/sumVectors.c
@@ -17,16 +17,21 @@ struct arg_ {
     sem_t *mutex;
     int init;
     int end;
+    int released;
 };
 
 void *preencheVetores( void *arguments ) {
     struct arg_ *args = ( struct arg_ * ) arguments;
+
+    sem_wait(args->mutex);
+
     for ( int i = args->init; i <= args->end;i++) {
-        sem_wait(args->mutex);
         *( args->vectorX + i ) = 1.0 + (( float ) rand() / RAND_MAX);
         *( args->vectorY + i ) = 1.0 + (( float ) rand() / RAND_MAX);
-        sem_post(args->mutex);
     }
+
+    args->released = 1;
+    sem_post(args->mutex);
 }
 
 void printVector( float *vector, int size ) {
@@ -38,19 +43,19 @@ void printVector( float *vector, int size ) {
 void *sumVector( void *arguments ) {
     struct arg_ *args = ( struct arg_ * ) arguments;
 
-    for ( int i = args->init; i <= args->end; i++) {
-        sem_wait(args->mutex);
-        // while (! *( args->vectorX + i) && ! *( args->vectorY + i)) {}
-        *( args->vectorZ + i ) = *( args->vectorX + i ) + *( args->vectorY + i );
-        sem_post(args->mutex);
-    }
-}
+    while (!args->released) {}
 
+    sem_wait(args->mutex);
+    for ( int i = args->init; i <= args->end; i++) {
+        *( args->vectorZ + i ) = *( args->vectorX + i ) + *( args->vectorY + i );
+    }
+    sem_post(args->mutex);
+}
 
 void main() {
     clock_t tempo;
-    num_threads = 16;
     size = 160000000;
+    num_threads = 16;
     int ubuntu = (size/num_threads);
 
     vecX = ( float* ) malloc( sizeof( float ) * (size) );
@@ -64,7 +69,9 @@ void main() {
     pthread_t * threadsS = ( pthread_t * ) malloc( num_threads* sizeof( pthread_t ) );
 
     struct arg_ *args = ( struct arg_ * ) malloc( num_threads* sizeof( struct arg_ ) );
+
     tempo = clock();
+
     for( int i = 0; i < num_threads; i++ ) {
         sem_init( &mutex[i], 0, 1 );
 
@@ -74,7 +81,8 @@ void main() {
         args[i].mutex = (mutex + i);
         args[i].init = i * ubuntu;
         args[i].end = ((i+1) * ubuntu) -1;
-        
+        args[i].released = 0;
+    
         pthread_create( &threadsP[i], NULL, preencheVetores, (void *) & args[i] );
         pthread_create( &threadsS[i], NULL, sumVector, ( void * ) & args[i] );
         sem_destroy( &mutex[i]);
@@ -86,9 +94,9 @@ void main() {
     }
 
     printf( "\n Time:%f\n",( clock() - tempo ) / ( double ) CLOCKS_PER_SEC );
+    // printVector(vecZ, size);
     pthread_exit( NULL );
     // printVector(vecX, size);
 
     // printVector(vecY, size);
-    // printVector(vecZ, size);
 }
