bbc6120bf8acb393b06ff14c337bc84774349ab7
Cangerana <mrjoaovictorc@gmail.com>
2021-06-27 21:36:26 -0300

    Nova versao final

--- SumVectors/sumVectors.c
+++ SumVectors/sumVectors.c
@@ -17,56 +17,62 @@ struct t_arg {
     float *vectorX;
     float *vectorY;
     float *vectorZ;
-    sem_t *mutex;
+    pthread_mutex_t *mutex;
 };
 
-void *preencheVetores( void *arguments ) {
+void *fiuVector( void *arguments ) {
     struct t_arg *args = ( struct t_arg * ) arguments;
 
-    sem_wait( args->mutex );
-    
+    pthread_mutex_lock( args->mutex );
+
     for ( int i = args->init; i <= args->end;i++) {
         *( args->vectorX + i ) = 1.0 + (( float ) rand() / RAND_MAX );
         *( args->vectorY + i ) = 1.0 + (( float ) rand() / RAND_MAX );
     }
 
+    pthread_mutex_unlock( args->mutex );
+
     args->released = 1;
-    sem_post(args->mutex);
 }
 
 void printVector( float *vector, int size ) {
-    for( int i = 0; i < size; i++ ) {
+    for( int i = 0; i < size; i++ )
         printf( "Vector[%i] : %f\n", i, vector[i] );
-    }
 }
 
 void *sumVector( void *arguments ) {
     struct t_arg *args = ( struct t_arg * ) arguments;
 
     while ( !args->released ) {} // Verifing if that portion of vector is fill
+    
+    pthread_mutex_lock( args->mutex);
 
-    sem_wait(args->mutex);
-    for ( int i = args->init; i <= args->end; i++ ) {
+    for ( int i = args->init; i <= args->end; i++ )
         *( args->vectorZ + i ) = *( args->vectorX + i ) + *( args->vectorY + i );
-    }
-    sem_post( args->mutex );
+
+    pthread_mutex_unlock( args->mutex );
 }
 
 void main() {
-    // printf( "TYPE VECTOR SIZE >> " );
-    // scanf( "%d", &size );
+    while (1) {
+        printf( "TYPE VECTOR SIZE >> " );
+        scanf( "%d", &size );
 
-    // printf( "TYPE AMOUNT OF THREADS >> " );
-    // scanf( "%d", &amt_threads );
-    size = 160000000; // 25.390
-    amt_threads = 8; 
-    int portion = ( size / amt_threads);
+        printf( "TYPE AMOUNT OF THREADS >> " );
+        scanf( "%d", &amt_threads );
+        if ( size % amt_threads == 0 )
+            break;
+        else
+            printf( "Size of vector need be divisable per amount of threads!\n" );
+    }
+
+    int portion = ( size / amt_threads );
 
     vecX = ( float* ) malloc( sizeof( float ) * (size) );
     vecY = ( float* ) malloc( sizeof( float ) * (size) );
     vecZ = ( float* ) malloc( sizeof( float ) * (size) );
 
-    sem_t *mutex = ( sem_t * ) malloc( amt_threads* sizeof( sem_t ) );
+    pthread_mutex_t *mutex = ( pthread_mutex_t * ) malloc( amt_threads* sizeof( pthread_mutex_t ) );
 
     pthread_t *threadsP = ( pthread_t * ) malloc( amt_threads* sizeof( pthread_t ) );
 
@@ -75,10 +81,10 @@ void main() {
     struct t_arg *args = ( struct t_arg * ) malloc( amt_threads* sizeof( struct t_arg ) );
 
     time_t start,end;
-    time (&start);
-    printf("IM STARTING");
+    time( &start );
+
     for ( int i = 0; i < amt_threads; i++ ) {
-        sem_init( &mutex[i], 0, 1 );
+        pthread_mutex_init(&mutex[i], NULL);
 
         args[i].vectorX = vecX;
         args[i].vectorY = vecY;
@@ -89,11 +95,9 @@ void main() {
         args[i].released = 0;
 
         printf(" The thread %i will SUM from %i to %i \n",i, args[i].init, args[i].end);
-        
-        pthread_create( &threadsP[i], NULL, preencheVetores, ( void * ) & args[i] );
-        pthread_create( &threadsS[i], NULL, sumVector, ( void * ) & args[i] );
 
-        sem_destroy( &mutex[i]);
+        pthread_create( &threadsP[i], NULL, fiuVector, ( void * ) & args[i] );
+        pthread_create( &threadsS[i], NULL, sumVector, ( void * ) & args[i] );
     }
 
     for( int i = 0; i < amt_threads; ++i ) {
@@ -101,11 +105,11 @@ void main() {
         pthread_join( threadsS[i], NULL );
     }
 
-    time (&end);
+    time( &end );
 
-    float dif = difftime (end,start);
+    float dif = difftime( end, start );
 
-    printf( "\n Time: %.3lf Seconds\n", dif);
+    printf( "\n Time: %.3lf Seconds\n", dif );
 
     // printVector(vecZ, size); // Uncomment if wanna see the result
 
